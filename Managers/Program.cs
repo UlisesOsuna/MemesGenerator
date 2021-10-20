c0456522ad68784ee88f4651d624dc40d3534ca8
UlisesOsuna <92827295+UlisesOsuna@users.noreply.github.com>
2021-10-20 15:39:30 -0500

    2021-10-19

---
+++ Managers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Managers {
+	public class Program {
+          public static void Main(string[] args) {
+               //Console.WriteLine("Hello World!");
+
+               //var services = new ServiceCollection();
+               //services.AddInternalServices();
+               //services.AddCsvFileConnector();
+
+               //var provider = services.BuildServiceProvider();
+
+               //var writer = provider.GetService<IWriteSimpleDataService>();
+               //writer.write();
+
+               //Console.ReadLine();
+          }
+     }
+}
